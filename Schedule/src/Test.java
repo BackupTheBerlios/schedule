a9757dd311eb121f000550b6397163514c88a028
rapid3 <rapid3>
2005-07-26 15:36:17 +0000

    Test-Case "Projekte zu einem User auslesen"

--- Schedule/src/Test.java
+++ Schedule/src/Test.java
@@ -17,6 +17,8 @@ import java.util.List;
 import com.schedule.hibernate.*;
 import com.schedule.jsfbeans.*;
 
+import net.sf.hibernate.*;
+
 public class Test {
 
 	/**
@@ -30,6 +32,20 @@ public class Test {
 	{
 		
 		//Hier den Test-Case implementieren
+		Session session = HibernateManager.getSession();
+		List userList = null;
+		Query q;
 		
+		try {
+			q = session.createQuery("from User where idUser= :idUser");
+			q.setString("idUser", "15");
+			userList = (List) q.list();			
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+		User user = (User) userList.get(0);
+		java.util.Set projects = user.getProjects();
+		Object[] projekte = projects.toArray();
+		System.out.println("Name des Projekts: " +((Projects)projekte[0]).getName());
 	}
 }
